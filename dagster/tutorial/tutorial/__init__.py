cea039f89c76863b35dadcdda16ce6b7cac76b7a
Santiago Abad <santiago.abad@fortris.com>
2024-04-01 09:04:22 +0200

    feat: Add hackernew_job schedule to run every hour

--- dagster/tutorial/tutorial/__init__.py
+++ dagster/tutorial/tutorial/__init__.py
@@ -1,6 +1,7 @@
 from dagster import (
     AssetSelection,
     Definitions,
+    ScheduleDefinition,
     define_asset_job,
     load_assets_from_modules,
 )
@@ -11,6 +12,12 @@ all_assets = load_assets_from_modules([assets])
 
 # Addition: define a job that will materialize the assets
 hackernews_job = define_asset_job("hackernews_job", selection=AssetSelection.all())
+
+# Addition: a ScheduleDefinition the job it should run and a cron schedule of how frequently to run it
+hackernews_schedule = ScheduleDefinition(
+    job=hackernews_job,
+    cron_schedule="0 * * * *",  # every hour
+)
 
 defs = Definitions(
     assets=all_assets,
